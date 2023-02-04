2def5b23ab7d8b706386be0bb73e06500e878d2c
Guerreiro <luguerreiro.souza@gmail.com>
2023-02-04 17:52:34 -0300

    Projeto base

---
+++ IC - Projeto/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    [SerializeField]
+    private float turboSpeed;
+
+    //Speed Turbo
+    private float minSpeed;
+    private float maxSpeed;
+
+    void Start()
+    {
+        minSpeed = speed;
+        maxSpeed = speed + turboSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        AstronautMovement();
+        AstronautTurbo();
+    }
+
+    void AstronautTurbo()
+    {
+        if (Input.GetAxis("RT") > 0f)
+        {
+            if(speed.Equals(minSpeed))
+            {
+                speed = maxSpeed;
+            }
+        }
+        else
+        {
+            if (speed.Equals(maxSpeed))
+            {
+                speed = minSpeed;
+            }         
+        }
+    }
+
+    void AstronautMovement()
+    {
+        //Sides
+        if (Input.GetAxis("Vertical") > 0f)
+        {
+            transform.position += Vector3.forward * speed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Vertical") < 0f)
+        {
+            transform.position += Vector3.back * speed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Horizontal") < 0f)
+        {
+            transform.position += Vector3.left * speed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        }
+
+        //Down and Up
+        if(Input.GetAxis("A") > 0)
+        {
+            transform.position += Vector3.up * speed * Time.deltaTime;
+        }
+        if (Input.GetAxis("Y") > 0)
+        {
+            transform.position += Vector3.down * speed * Time.deltaTime;
+        }
+    }
+}
